ee2820c28d68689ffc5aadf5b1edf825a4bf8747
Vibhuair20 <114051581+Vibhuair20@users.noreply.github.com>
2025-02-24 21:59:00 +0530

    2nd commit- getlinks

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func analyze(url, baseURL string, visited* map[string]string){
@@ -14,6 +17,78 @@ func analyze(url, baseURL string, visited* map[string]string){
 		fmt.Printf("error fetching page%s: %v/n", url, err)
 		return
 	}
+	// getting the page tilte
+	title := pageTitle(page)
+
+	// store url and title in the map
+	(*visited)[url] = title
+
+	// get all the links in the page
+	// using nil becsause we are using recursion here
+	links := GetLinks(nil, page)
+
+	//visiting each link from the same page
+	for _, link := range link{
+		if(*visited)[link] == "" && strings.HasPrefix(link, baseURL){
+			analyze(link, baseUrl, visited)
+		}
+	}
+}
+
+// parse fetches and parses an html page
+func parse(url string) (*html.Node, error){
+	// making a http get request
+	resp, err := http.Get(url)
+	if err != nil{
+		return nil, fmt.Errorf("cannot parse pages")
+	}
+	// closing the body
+	defer resp.Body.Close()
+
+	// parsing the html
+	node, err := html.Parse(resp.Body)
+	if err != nil{
+		return nil, fmt.Errorf("cannot parse pages")
+	}
+	return node, nil
+}
+
+// page title extracts from the html page
+func pageTitle(n string) (*html.Node){
+	// base case
+	if n.Type == html.ElementNode && n.Data == "title"{
+		return n.FirstChild.Data
+	}
+
+	// recursivley for the children nodes
+	for c := n.FirstChild; c != nil; c.NextSibling{
+		if title := pageTitle(c); title != ""{
+			return title
+		}
+	}
+	return "No Title"
+}
+
+// page links extracts all the linsks from the page
+func GetLinks(links []string, n *html.Node) []string{
+	// if current node is the anchor tag
+	if n.Type == html.ElementNode && n.Data == "a"{
+		// loo for href attribute
+		for _, attr := range n.attr{
+			if attr.Key == "href" {
+                // Add link if it's not already in the slice
+                if !sliceContains(links, attr.Val) {
+                    links = append(links, attr.Val)
+                }
+            }
+		}
+	}
+
+	//recursove for children nowlinks
+	for c := n.FirstChild; c != nil; c.NextSibling{
+		links = pageLinks(links, c)
+	}
+	return links
 }
 
 // this is the main function
